dff4f5aa68a9faefb47bc7d34207049aa36b6c4d
anuradharompally <anuradha.07anu@gmail.com>
2020-12-26 18:44:33 +0530

    adding Amazon files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBzPis2c3Joctmu5bJ8xy_1_mwIhkOjE1Q",
+    authDomain: "fir-48b8c.firebaseapp.com",
+    projectId: "fir-48b8c",
+    storageBucket: "fir-48b8c.appspot.com",
+    messagingSenderId: "689470342733",
+    appId: "1:689470342733:web:3dd252aa7230df058fbe04",
+    measurementId: "G-M4PP0BBXL1"
+  };
